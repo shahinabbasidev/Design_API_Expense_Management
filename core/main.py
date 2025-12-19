4ab4e1666b1c3a49613bae55d8960a2d8edd1b61
shahinabbasidev <shahin.abbasi.dev@gmail.com>
2025-12-19 18:33:46 +0330

    write the CRUD functions

---
+++ core/main.py
@@ -0,0 +1,48 @@
+from fastapi import FastAPI,HTTPException,status
+from fastapi.responses import JSONResponse
+import random
+
+
+app = FastAPI()
+
+
+expenses_list = []
+
+
+@app.post("/expenses")
+def create_expense(title:str,mount:float):
+    expense_obj = {"id":random.randint(1,1000),"title":title,"mount":mount}
+    expenses_list.append(expense_obj)
+    return JSONResponse(content=expense_obj,status_code=status.HTTP_201_CREATED)
+
+@app.get("/expenses")
+def get_all_expenses():
+    return expenses_list
+
+
+@app.get("/get_expenses/{id}")
+def get_expense(id:int):
+    for item in expenses_list:
+        if item["id"] == id:
+            return JSONResponse(content=item,status_code=status.HTTP_200_OK)
+    return HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="object not found")
+
+@app.put("/expenses/{id}")
+def update_list(id:int,title:str):
+    for item in expenses_list:
+        if item["id"] == id:
+            item["title"] = title
+            return JSONResponse(content={"detail":"object update successfully"},status_code=status.HTTP_200_OK)
+    return HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="object not found")
+
+
+@app.delete("/delete/{id}")
+def delete_title(id:int):
+    for item in expenses_list:
+        if item["id"] == id:
+            expenses_list.remove(item)
+            return JSONResponse(content={"detail":"object remove successfully"},status_code=status.HTTP_200_OK)
+    return HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="object not found")
+
+
+
